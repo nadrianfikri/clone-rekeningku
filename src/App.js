6b3d26049a6f8ee068c8c31cdb82b9770c2cc297
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-12-14 18:38:12 +0700

    add tailwindcss and some library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="bg-gradient-to-l from-violet-400 to-rose-500  text-slate-200 w-full p-4 font-bold shadow-lg shadow-violet-500/60">Home page</div>;
 }
 
 export default App;
